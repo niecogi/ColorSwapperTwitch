424fb046985c31aa31e78aeb7ade315d09668915
Nieves <nievescg98@gmail.com>
2022-10-21 20:18:51 +0200

    pink save

--- setTheme.js
+++ setTheme.js
@@ -1,2 +1,25 @@
 document.querySelector('html').classList.add('twitch-theme')
 
+const menuButton = document.querySelector('[data-test-selector="user-menu__toggle"]')
+menuButton.addEventListener('click', () => {
+  setTimeout(() => {
+    const menuWrapper = document.querySelector('[data-test-selector="user-menu-dropdown__main-menu"]')
+
+    const darkThemeOption = menuWrapper.querySelector('.Layout-sc-nxg1ff-0.FTFzP').children[2]
+    const div = document.createElement('div')
+    const stringHTML = `
+     <div class="Layout-sc-nxg1ff-0 dwuicp">
+      <div class="Layout-sc-nxg1ff-0 gcwIMz">
+      
+      </div>
+    </div>
+`
+    darkThemeOption.after(div)
+    div.innerHTML = stringHTML
+  }, 500)
+
+
+})
+
+
+
